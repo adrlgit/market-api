3d86c00e227b97d78bc3c169c2c10afcea6c47aa
Andre Luis <luis.alms91@gmail.com>
2019-11-17 12:32:29 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/andreluis/market/domain/Categoria.java
+++ src/main/java/com/andreluis/market/domain/Categoria.java
@@ -4,14 +4,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 import java.util.Objects;
 
-@Getter @Setter @NoArgsConstructor
+@Getter @Setter @NoArgsConstructor @Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
