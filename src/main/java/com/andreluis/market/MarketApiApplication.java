0fa5648ff07ccf8230082d0287dc39208c7b8bbc
Andrré <luis.alms91@gmail.com>
2019-11-28 22:26:24 -0300

    Estado e Cidade

--- src/main/java/com/andreluis/market/MarketApiApplication.java
+++ src/main/java/com/andreluis/market/MarketApiApplication.java
@@ -1,8 +1,12 @@
 package com.andreluis.market;
 
 import com.andreluis.market.domain.Categoria;
+import com.andreluis.market.domain.Cidade;
+import com.andreluis.market.domain.Estado;
 import com.andreluis.market.domain.Produto;
 import com.andreluis.market.repositories.CategoriaRepository;
+import com.andreluis.market.repositories.CidadeRepository;
+import com.andreluis.market.repositories.EstadoRepository;
 import com.andreluis.market.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +23,12 @@ public class MarketApiApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApiApplication.class, args);
@@ -43,5 +53,20 @@ public class MarketApiApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est2 );
+		Cidade c2 = new Cidade(null, "São Paulo", est1);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+
 	}
 }
