a5ac0f4df3c606e37f24f0e7aa03a78d58e71ed6
Andrré <luis.alms91@gmail.com>
2019-12-07 12:36:34 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/andreluis/market/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.andreluis.market.repositories;
+
+import com.andreluis.market.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
